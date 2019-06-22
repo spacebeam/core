f57952fb6dfee49364ad00a93fc45481c62fcf4b
Jean Chassoul <jchassoul@gmail.com>
2019-06-22 08:55:43 -0600

    clean, clean, clean

--- src/bw.lua
+++ src/bw.lua
@@ -1,13 +1,10 @@
 #!/usr/bin/env luajit
-
 --
 -- StarCraft:Brood War bots running inside a Singularity Linux® Container
 --
-
 local argparse = require("argparse")
 local socket = require("socket")
 local uuid = require("uuid")
-
 -- init random seed
 uuid.randomseed(socket.gettime()*10000)
 -- Session UUID
@@ -19,29 +16,19 @@ local parser = argparse() {
    epilog = "It can download and launch Win32 C++ and Java bots " .. 
    "or any Linux® bot with support for BWAPI 4.1.2, 4.2.0, 4.4.0."
 }
-
 -- Spawning bots at directory
 parser:option("-d --directory", "StarCraft bots directory", "/opt/StarCraft/bots/")
 -- Fighting bots
 parser:option("-b --bots", "Prepare to fight", "Ophelia Blueberry")
 -- Map is not territory
 parser:option("-m --map", "for territory", "maps/download/Fighting\\ Spirit.scx")
-
---
 -- CLI pkg command
-
-
 parser:command_target("command")
-
--- ?
+-- How are you? 
 parser:command("status")
-
--- execute the game
+-- Live for the swarm! 
 parser:command("play")
-
--- Your system variables
-
--- system messages
+-- Your system messages
 local messages = {
   'Can I take your order?',
   'Go ahead HQ.',
@@ -49,7 +36,6 @@ local messages = {
   'In transit HQ.',
   'I copy that.',
 }
-
 -- Parse your arguments
 local args = parser:parse()
 -- STATUS, STATUS, STATUS 
