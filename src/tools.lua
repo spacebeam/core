021c807600770bc7cc558adc7ff6a2d4b9db7f09
Jean Chassoul <jchassoul@gmail.com>
2019-07-24 11:44:55 -0600

    clean, clean, clean

--- src/tools.lua
+++ src/tools.lua
@@ -1,12 +1,8 @@
 --
---
+-- Everyone's tool library
 --
 
 local tools = {}
-
-function tools.this_is()
-    print("this is only a test")
-end
 
 function tools.read_file(file)
     local f = assert(io.open(file, "rb"))
