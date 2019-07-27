776ece9ddce2f9d73a4b0b71cc1ff5ab2aec7f0f
Jean Chassoul <jchassoul@gmail.com>
2019-07-26 20:54:40 -0600

    clean, clean, clean

--- src/conf.lua
+++ src/conf.lua
@@ -11,7 +11,9 @@ print(conf)
 
 -- add work on bwapi.ini
 local ini = require("inifile")
+
 -- BWAPI and TorchCraft use .ini files
+bwapi = ini.parse("../include/bwapi-data/bwapi.ini")
 
 -- Your local variables
 local directory = conf["starcraft"]
@@ -29,6 +31,15 @@ local bwapi_write = bwapi_data .. "write"
 local bwapi_ai = bwapi_data .. "AI"
 local bwapi_logs = bwapi_data .. "logs"
 
+-- BWAPI version 4.2.0 and higher ONLY
+-- FIRST (default), use the first character in the list
+-- WAIT, stop at this screen
+-- else the character with the given value is used/created
+local character_name = "FIRST"
+-- BWAPI version 4.2.0 and higher ONLY
+-- Text that appears in the drop-down list below the Game Type.
+local game_type_extra = ""
+
 print(bots)
 
 print(games)
@@ -36,3 +47,5 @@ print(games)
 print(maps)
 
 print(bwapi_ai)
+
+print(bwapi)
