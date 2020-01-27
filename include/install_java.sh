6c2398b51d0bc64d7b77e14a3c5c93a997268997
Jean Chassoul <jchassoul@gmail.com>
2020-01-27 12:28:28 -0600

    clean, clean, clean

--- include/install_java.sh
+++ include/install_java.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function install_java() {
-    cd /usr/src
+    cd ~ 
     wget https://repo.huaweicloud.com/java/jdk/8u192-b12/jdk-8u192-windows-i586.exe 
-    wine jdk-8u192-windows-i586.exe /s
+    sudo -u wine wine jdk-8u192-windows-i586.exe /s
     REG_KEY="HKEY_CURRENT_USER\Environment"
-    wine REG ADD "${REG_KEY}" /v PATH /t REG_EXPAND_SZ /d "C:\Program Files (x86)\Java\jre1.8.0_192\bin" /f
+    sudo -u wine wine REG ADD "${REG_KEY}" /v PATH /t REG_EXPAND_SZ /d "C:\Program Files (x86)\Java\jre1.8.0_192\bin" /f
 }
 
 install_java
