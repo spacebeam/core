bb8fd78e52d9d87a0eb18cb13bd00cc96b807b79
Jean Chassoul <jchassoul@gmail.com>
2020-07-14 15:30:47 -0600

    clean, clean, clean

--- examples/launcher.py
+++ examples/launcher.py
@@ -4,19 +4,38 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(descroption='host a local pc game with bwheadless')
+parser = argparse.ArgumentParser(descroption='host a game with bwheadless')
 
-parser.add_argument('-p', '--path', type='str', default='/opt/StarCraft/', help='StarCraft path')
-parser.add_argument('-b', '--bot', type='str', default='blueberry')
-parser.add_argument('-r', '--race', type='str', default='Terran')
-parser.add_argument('-m', '--map', type='str', default='\(4\)FightingSpirit1.3.scx')
+parser.add_argument('-p', '--path',
+                    type='str',
+                    default='/opt/StarCraft/',
+                    help='StarCraft path')
+parser.add_argument('-b', '--bot',
+                    type='str',
+                    default='blueberry')
+parser.add_argument('-r', '--race',
+                    type='str',
+                    default='Terran')
+parser.add_argument('-m', '--map',
+                    type='str',
+                    default='\(4\)FightingSpirit1.3.scx')
 
 args = parser.parse_args()
 
-```
+path = args.path
+
+bot = args.bot
+
+race = args.race
+
+map = args.map
+
+execute = '''
 wine bwheadless.exe -e /opt/StarCraft/StarCraft.exe\
  -l /opt/StarCraft/bwapi-data/BWAPI.dll --host\
  --name blueberry --game blueberry --race T\
  --map maps/TorchUp/\(4\)FightingSpirit1.3.scx&\
 wine Chaoslauncher/Chaoslauncher.exe
-```
+'''
+
+print(execute)
