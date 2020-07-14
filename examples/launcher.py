2a5ceda1f8e3b87a502c5893fd43a9107a1fe97c
Jean Chassoul <jchassoul@gmail.com>
2020-07-14 12:01:25 -0600

    clean, clean, clean

--- examples/launcher.py
+++ examples/launcher.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+
 # launch bwheadless and chaoslauncher from here!
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument()
+parser.add_argument()
+
+args = parser.parse_args()
+
+```
+wine bwheadless.exe -e /opt/StarCraft/StarCraft.exe\
+ -l /opt/StarCraft/bwapi-data/BWAPI.dll --host\
+ --name blueberry --game blueberry --race T\
+ --map maps/TorchUp/\(4\)FightingSpirit1.3.scx&\
+wine Chaoslauncher/Chaoslauncher.exe
+```
