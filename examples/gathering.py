e8e91347cca5bd6f280f4ffca582923d3af12f11
Jean Chassoul <jchassoul@gmail.com>
2020-07-23 16:14:57 -0600

    clean, clean, clean

--- examples/gathering.py
+++ examples/gathering.py
@@ -1,4 +1,4 @@
-# learning to use the tochcraft python client!
+# Learning to use the TochCraft Python 3 client!
 
 import torchcraft as tc
 import torchcraft.Constants as tcc
@@ -18,38 +18,27 @@ def get_closest(x, y, units):
 bot = {'id': 0, 'name': 'Blueberry'}
 
 skip_frames = 7
-
 # check past
 built_barracks = 0
-
 # active doing
 building_supply = False
 # producing what friend?
 producing = False
-
 # units
 units = None
 workers = 0
-
 # buildings
 supply_depots = 0
 barracks = 0
 command_centers = 0
-factories = 0
-starports = 0
-turrets = 0
-engineering_bay = 0
-academy = 0
-armory = 0
-bunkers = 0
 refineries = 0
 
 
-# TODO: isworker, filter_type, isbuilding
+# TODO: isworker, isbuilding
 
 # TODO: command2order unitcommandtypes, command_unit
 
-# TODO: pretty print the state and compare with the javascript data structure
+# TODO: log state and compare the javascript data structure
 
 while True:
     print("CTRL-C to stop")
@@ -80,19 +69,14 @@ while True:
             units = state.units[bot['id']]
             enemy = state.units[bot['enemy']]
             if state.battle_frame_count % skip_frames == 0:
-
                 used_psi = state.frame.resources[bot['id']].used_psi
                 total_psi = state.frame.resources[bot['id']].total_psi
-
                 if used_psi != total_psi:
                     building_supply = False
-
                 # ok go!
                 for unit in units:
-
                     if tc.Constants.unittypes._dict[unit.type] == 'Terran_SCV':
                         workers.append(unit.id)
-
                     # check if building do [a, b, c] else worker do [x, y, z]
                     if tc.Constants.unittypes._dict[unit.type]\
                             == 'Terran_Command_Center':
@@ -113,9 +97,7 @@ while True:
                             ])
                             # to train a unit you MUST input into "extra" field
                             producing = True
-
                     print(tc.Constants.unittypes._dict.get(unit.type))
-
                     target = get_closest(unit.x, unit.y, enemy)
                     if target is not None:
                         actions.append([
@@ -124,18 +106,10 @@ while True:
                             tcc.unitcommandtypes.Attack_Unit,
                             target.id,
                         ])
-
                     # else and just else... gather some resources? wtf xD
-
                 print(workers)
-
         print("Sending actions: {}".format(str(actions)))
         print(state.map_name)
-        print(state.map_size)
-        # print(state.start_locations)
-        print(state.frame.resources[bot['id']].ore)
         print(state.frame.resources[bot['id']].gas)
-        # print(state.frame.resources[bot['id']].used_psi)
-        # print(state.frame.resources[bot['id']].total_psi)
         client.send(actions)
     client.close()
